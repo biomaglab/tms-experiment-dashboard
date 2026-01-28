1abdb9f024ded1ef5055bb598e539e24afb46573
Marcio Campos <82606834+MarcioCamposJr@users.noreply.github.com>
2026-01-28 17:36:15 -0300

    FIX: import function to create 3d navigation

--- src/tms_dashboard/nicegui_app/ui/__init__.py
+++ src/tms_dashboard/nicegui_app/ui/__init__.py
@@ -4,6 +4,8 @@
 
 from .header import create_header
 from .dashboard_tabs import create_dashboard_tabs
+from .navigation_3d import create_3d_scene_with_models
+from .checklist_tab import create_checklist_tab
 
 __all__ = [
     'create_header',
